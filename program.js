83b73c83b6d0a5056dc8a143dcdb0f9a31c4857d
Giuseppe Crinò <giuscri@gmail.com>
2014-08-28 22:40:59 +0200

    Bleah. Does not work on my machine, but ✓

--- program.js
+++ program.js
@@ -1,1 +1,10 @@
-// Buuuh!, have no idea!! *angry people around*
+var http = require('http')
+var fs = require('fs')
+var args = process.argv.slice(2)
+var file = args[0]
+var port = Number(args[1])
+var server = http.createServer(function (req, res) {
+	res.writeHead(200, {'Content-Type': 'text/plain'})
+	fs.createReadStream(file).pipe(res)
+})
+server.listen(port)
